46ce78aa5f640eaef28d153e140be4db5be6ec4a
Apsara Bishwokarma <apsarabishwokarma7@gmail.com>
2022-12-30 22:53:26 +0545

    folder created

---
+++ Basics_of_python/week-first/variables.py
@@ -0,0 +1,29 @@
+# Variables - > storagge or aplace where we store data
+# It is more location to store data
+# it is container
+
+a = 4
+b = 3
+total = a + b  # 4 + 3 =  7
+diff = a - b  # 4 - 1 = 1
+product = a * b  # 4 * 3 = 12
+div = a / b  # 4 / 3 = 1.33
+remainder = a % b  # 4 % 3 = 1
+exponential = a ** b  # 4 ** 3 = 64
+
+print('total:', total)
+print('difference: ', diff)
+print('product: ', product)
+print('division:', div)
+print('remainder: ', remainder)
+print('exponentional: ', exponential)
+
+first_name = 'Apsara'
+last_name = 'Bishwokarma'
+age = 20
+country = 'Nepal'
+city = 'kathmandu'
+current_year = 2022
+birth_year = 2002
+age = current_year - birth_year
+print(age)
